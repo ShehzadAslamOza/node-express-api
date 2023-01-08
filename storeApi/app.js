2f2008d166a06d7ee7f7f61e98319f2cbb38b67f
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-01-08 16:00:24 +0500

    Setup basic routes, connected to DB and setup express-async-errors

--- storeApi/app.js
+++ storeApi/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+// product routes
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`The Server is Listening at ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
